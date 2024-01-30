b00cd35f950e9a6ecdb146948162184e0f03dbc2
ayoub <sb.naba2@gmail.com>
2024-01-30 23:54:20 +0100

    added parent null case

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,6 +10,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (parent == NULL)
+		return (NULL);
 	if (parent->right == NULL)
 	{
 		parent->right = binary_tree_node(parent, value);
