81a129f32dcd1af5de9744ac98d4cc7001a33867
xtreme <anastaai94@gmail.com>
2024-01-30 18:51:27 +0100

    fixed betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - inserts a node as the right-child of another node
- *@parent: parent node
- *@value: int value in node
- * Return: pointer to node or NULL
+ * binary_tree_is_leaf - checks if a node is a leaf
+ *@node: the node
+ * Return: 1 if leaf 0 if not
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
